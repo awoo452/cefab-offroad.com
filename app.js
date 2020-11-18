e70e8872ce7c3d67d46674f13addff7a3d07dd96
Aaron <awoo452@wgu.edu>
2020-11-17 23:18:37 -0800

    Added to the scrollable image bin, addressed some display issues

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
+const dots = document.querySelectorAll('.dot');
 const auto = true;
 const intervalTime = 5000;
 let slideInterval;
+let dotInterval;
 
 const nextSlide = () => {
     const current = document.querySelector('.current');
@@ -14,6 +16,16 @@ const nextSlide = () => {
        slides[0].classList.add('current');
     }
     setTimeout(() => current.classList.remove('current'));
+}
+const nextDot = () => {
+    const currentDot = document.querySelector('.currentDot');
+    currentDot.classList.remove('currentDot');
+    if (currentDot.nextElementSibling) {
+        currentDot.nextElementSibling.classList.add('currentDot');
+    } else {
+       dots[0].classList.add('currentDot');
+    }
+    setTimeout(() => currentDot.classList.remove('currentDot'));
 }
 
 const prevSlide = () => {
@@ -26,23 +38,40 @@ const prevSlide = () => {
     }
     setTimeout(() => current.classList.remove('current'));
 }
+const prevDot = () => {
+    const currentDot = document.querySelector('.currentDot');
+    currentDot.classList.remove('currentDot');
+    if (currentDot.previousElementSibling) {
+        currentDot.previousElementSibling.classList.add('currentDot');
+    } else {
+       dots[dots.length - 1].classList.add('currentDot');
+    }
+    setTimeout(() => currentDot.classList.remove('currentDot'));
+}
 
 next.addEventListener('click', e => {
     nextSlide();
+    nextDot();
     if (auto) {
         clearInterval(slideInterval);
+        clearInterval(dotInterval);
         slideInterval = setInterval(nextSlide, intervalTime);
+        dotInterval = setInterval(nextDot, intervalTime);
     }
 });
 
 prev.addEventListener('click', e => {
     prevSlide();
+    prevDot();
     if (auto) {
         clearInterval(slideInterval);
+        clearInterval(dotInterval);
         slideInterval = setInterval(nextSlide, intervalTime);
+        dotInterval = setInterval(nextDot, intervalTime);
     }
 });
 
 if(auto) {
     slideInterval = setInterval(nextSlide, intervalTime);
+    dotInterval = setInterval(nextDot, intervalTime);
 }
